7b1d20e7a9e3e91ab180dc3ca474aa9f698f8ab4
Konstantin Gordienko <bluesbaker@mail.ru>
2023-10-02 15:10:51 +0300

    Add format of output

--- cropimg.go
+++ cropimg.go
@@ -3,90 +3,86 @@ package main
 import (
 	"flag"
 	"fmt"
-	"image"
-	"image/jpeg"
-	"image/png"
 	"os"
 	"path/filepath"
 	"strings"
-)
+	"time"
 
-type SubImage interface {
-	SubImage(r image.Rectangle) image.Image
-}
+	"github.com/bluesbaker/cropimg/pkg/imageutil"
+)
 
 func main() {
 	// flags
 	imageSource := flag.String("source", "", "image(s) source")
-	postfix := flag.String("postfix", "_cropped", "cropped image postfix")
+	format := flag.String(
+		"format",
+		"{dir}/{name}_cropped.{ext}",
+		"output file format:\n"+
+			"\t{dir} - directory\n"+
+			"\t{name} - file name\n"+
+			"\t{ext} - file extension\n"+
+			"\t{time} - current time(24-59-59)\n"+
+			"\t{date} - current date(01.02.2003)\n"+
+			"\t{index} - file index\n")
 	width := flag.Int("width", 0, "width")
 	height := flag.Int("height", 0, "height")
-	leftOffset := flag.Int("left", 0, "left offset")
-	topOffset := flag.Int("top", 0, "top offset")
+	left := flag.Int("left", 0, "left offset")
+	top := flag.Int("top", 0, "top offset")
 	flag.Parse()
 
-	// check image source
+	// check image(s) source
 	if *imageSource == "" {
 		fmt.Printf("Image source '%s' is empty\n", *imageSource)
 		os.Exit(1)
 	}
 
+	// get image(s)
 	images, err := filepath.Glob(*imageSource)
 	if err != nil {
 		fmt.Println("Pattern is not readable", *imageSource)
 		os.Exit(1)
 	}
 
-	for _, img := range images {
-		cropImage(img, *width, *height, *leftOffset, *topOffset, *postfix)
+	// crop and save image(s)
+	for i, imagePath := range images {
+		// open
+		imageFile, imageExt, err := imageutil.Open(imagePath)
+		if err != nil {
+			fmt.Println("Open image error:", err)
+			os.Exit(1)
+		}
+
+		// crop
+		imageFile = imageutil.Crop(imageFile, *width, *height, *left, *top)
+
+		// save
+		filePath := formatFilePath(imagePath, *format, i+1)
+		imagePath, err := imageutil.Save(imageFile, imageExt, filePath)
+		if err != nil {
+			fmt.Println("Save image error:", err)
+			os.Exit(1)
+		}
+		fmt.Println(imagePath)
 	}
 }
 
-func cropImage(filePath string, width, height, left, top int, postfix string) {
-	var originalImage image.Image
-	ext := filepath.Ext(filePath)
-	fileName := strings.Split(filepath.Base(filePath), ext)[0]
+func formatFilePath(filePath, formatString string, index int) string {
+	now := time.Now()
+	timeNow := now.Format("15-04-05")
+	dateNow := now.Format("02.01.2006")
+	baseName := strings.Split(filepath.Base(filePath), ".")
+	name := baseName[0]
+	ext := baseName[1]
+	dir := filepath.Dir(filePath)
 
-	originalImageFile, err := os.Open(filePath)
-	if err != nil {
-		panic(err)
-	}
-	defer originalImageFile.Close()
+	formatedPath := formatString
+	formatedPath = strings.ReplaceAll(formatedPath, "{dir}", "%[1]s")
+	formatedPath = strings.ReplaceAll(formatedPath, "{name}", "%[2]s")
+	formatedPath = strings.ReplaceAll(formatedPath, "{ext}", "%[3]s")
+	formatedPath = strings.ReplaceAll(formatedPath, "{time}", "%[4]s")
+	formatedPath = strings.ReplaceAll(formatedPath, "{date}", "%[5]s")
+	formatedPath = strings.ReplaceAll(formatedPath, "{index}", "%[6]d")
+	formatedPath = fmt.Sprintf(formatedPath, dir, name, ext, timeNow, dateNow, index)
 
-	switch {
-	case ext == ".png":
-		originalImage, err = png.Decode(originalImageFile)
-		if err != nil {
-			panic(err)
-		}
-	case (ext == ".jpeg") || (ext == ".jpg"):
-		originalImage, err = jpeg.Decode(originalImageFile)
-		if err != nil {
-			panic(err)
-		}
-	default:
-		fmt.Printf("file extension is bad!")
-		os.Exit(1)
-	}
-
-	cropSize := image.Rect(0, 0, width, height)
-	cropSize = cropSize.Add(image.Point{left, top})
-	croppedImage := originalImage.(SubImage).SubImage(cropSize)
-
-	croppedImageFile, err := os.Create(filepath.Join(filepath.Dir(filePath), fmt.Sprintf("%s%s%s", fileName, postfix, ext)))
-	if err != nil {
-		panic(err)
-	}
-	defer croppedImageFile.Close()
-
-	switch {
-	case ext == ".png":
-		if err := png.Encode(croppedImageFile, croppedImage); err != nil {
-			panic(err)
-		}
-	case (ext == ".jpeg") || (ext == ".jpg"):
-		if err := jpeg.Encode(croppedImageFile, croppedImage, nil); err != nil {
-			panic(err)
-		}
-	}
+	return formatedPath
 }
